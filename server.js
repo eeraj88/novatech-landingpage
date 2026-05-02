98842acfd6ad491052e5b8125bb7fe5c84c45d6d
EerajJan <eeraj.jn@googlemail.com>
2026-05-02 13:13:48 +0200

    Fix: Add path module for static files

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('.'));
+app.use(express.static(path.join(__dirname, '.')));
 
 // GET / - Serve index.html
 app.get('/', (req, res) => {
